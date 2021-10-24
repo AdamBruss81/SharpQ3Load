19c0e6911ca7e7b788abf22ab51096f7705a7844
Adam Bruss <abrusszeno@gmail.com>
2021-10-24 17:18:11 -0500

    Cleanup zipper code namespace usings. Remove freeglut copy.

--- Engine/Zipping/Zipper.cs
+++ Engine/Zipping/Zipper.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using ICSharpCode.SharpZipLib.Zip;
-using ICSharpCode.SharpZipLib.Core;
 using System.IO;
 
 namespace engine

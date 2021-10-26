aba64d53b8febaeb410d4135ea401b08ffe01529
Adam Bruss <abrusszeno@gmail.com>
2021-10-25 22:10:15 -0500

    Add this testbed for testing things without having to run the main gamewindow.

---
+++ testbed_main/Program.cs
@@ -0,0 +1,14 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+
+namespace testbed_main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FastZip zipper = new FastZip();
+
+            zipper.ExtractZip(@"C:\Users\abruss\Downloads\simpsons_map\simpsons.pk3", @"c:\temp", "levelshots");
+        }
+    }
+}
